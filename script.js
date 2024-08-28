e3ed0d97a8efa1dd71c3c181025f24a924f10867
Benzauber <ben.leibundgut@gmail.com>
2024-08-28 13:49:42 +0200

    answer sound

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
+var audio = false;
+
+var correctAudio = new Audio('correct-answer.wav');
+var wrongAudio = new Audio('wrong-answer.wav');
+
 function checkAnswer(inputElement, answer) {
   var userAnswer = inputElement.value.trim();
   var feedbackElement = inputElement.nextElementSibling;
+  if (userAnswer !== ""){
+    if (!feedbackElement || !feedbackElement.classList.contains("feedback")) {
+      feedbackElement = document.createElement("div");
+      feedbackElement.classList.add("feedback");
+      inputElement.parentNode.appendChild(feedbackElement);
+    }
 
-  if (!feedbackElement || !feedbackElement.classList.contains("feedback")) {
-    feedbackElement = document.createElement("div");
-    feedbackElement.classList.add("feedback");
-    inputElement.parentNode.appendChild(feedbackElement);
-  }
-
-  if (userAnswer.toLowerCase() === answer.toLowerCase()) {
-    feedbackElement.className = "feedback correct";
-    feedbackElement.textContent = "Richtig!";
-  } else {
-    feedbackElement.className = "feedback incorrect";
-    feedbackElement.textContent = "Falsch! Die richtige Antwort lautet: " + answer;
+    if (userAnswer.toLowerCase() === answer.toLowerCase()) {
+      feedbackElement.className = "feedback correct";
+      feedbackElement.textContent = "Richtig!";
+      if(audio == true){
+        correctAudio.play();
+      }
+    } else {
+      feedbackElement.className = "feedback incorrect";
+      feedbackElement.textContent = "Falsch! Die richtige Antwort lautet: " + answer;
+      if(audio == true){
+        wrongAudio.play();
+      }
+    }
+  }else{
+    console.log("Empty")
   }
 }
 
@@ -35,5 +49,21 @@ function swapQA() {
     if (feedbackElement) {
       feedbackElement.parentNode.removeChild(feedbackElement);
     }
+  }
+}
+function mute(){
+  // Hole das Icon-Element
+  const icon = document.querySelector('.swap-button2 i');
+  // Überprüfe, ob das Icon aktuell auf "mute" steht
+  if (icon.classList.contains('fa-volume-mute')) {
+     // Ändere das Icon zu "volume-up"
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+    audio = true;
+  } else {
+    // Ändere das Icon zurück zu "mute"
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+    audio = false;
   }
 }
